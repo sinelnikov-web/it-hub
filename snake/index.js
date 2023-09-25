54a2753f03d429ded4eab66694a5f89a31e8eb7a
Максим <sinelnikov-web@mail.ru>
2023-09-25 19:04:35 +0600

    complete snake

--- snake/index.js
+++ snake/index.js
@@ -34,11 +34,23 @@ let directionDelta = {
     [Direction.RIGHT]: {x: 1, y: 0},
 }
 
+function getRandomCords(rangeStart = 0, rangeEnd = 25) {
+    return {
+        x: Math.floor(Math.random() * rangeEnd) + rangeStart,
+        y: Math.floor(Math.random() * rangeEnd) + rangeStart,
+    }
+}
+
 // game state
 let game = {
     map: document.querySelector('.game'),
     mapHeight: 25,
     mapWidth: 25,
+    speed: 60,
+    score: 0,
+    scoreElement: document.querySelector('.score'),
+    speedElement: document.querySelector('.speed'),
+    fruitsCountElement: document.querySelector('.fruits-count'),
     get mapCenterX() {
         return Math.floor(this.mapWidth / 2);
     },
@@ -46,24 +58,61 @@ let game = {
         return Math.floor(this.mapHeight / 2);
     },
     isStarted: false,
+    fruits: [
+        getRandomCords(),
+        getRandomCords(),
+    ],
 
     async start() {
         this.isStarted = true;
 
-        while (true) {
+        while (!this.isLose) {
             snake.move();
+            game.logic();
             game.draw();
-            await sleep(1000 / 20);
+            await sleep(1000 / this.speed);
+        }
+    },
+
+    logic() {
+        snake.tail.forEach((tail) => {
+            if (tail.x === snake.head.x && tail.y === snake.head.y) {
+                this.isLose = true;
+            }
+        });
+        for (let i = 0; i < this.fruits.length; i++) {
+            let fruitCords = this.fruits[i];
+            if (fruitCords.x === snake.head.x && fruitCords.y === snake.head.y) {
+                this.score++;
+                snake.tail.push({});
+                this.fruits.splice(i, 1);
+                this.fruits.push(getRandomCords());
+                break;
+            }
         }
     },
 
     draw() {
+        this.scoreElement.innerText = this.score;
+        this.speedElement.innerText = this.speed;
+        this.fruitsCountElement.innerText = this.fruits.length;
+
+        let fruitElements = document.querySelectorAll('.fruit');
+        fruitElements.forEach((element) => element.classList.remove(BlockType.FRUIT))
+
+        this.fruits.forEach((fruit) => {
+            let element = document.querySelector(
+                `[data-x="${fruit.x}"][data-y="${fruit.y}"]`
+            );
+            element.classList.add(BlockType.FRUIT);
+        });
+
         let headElement = document.querySelector('.head');
-        headElement.classList.remove('head');
+        headElement.classList.remove(BlockType.HEAD);
 
         let tailElements = document.querySelectorAll('.tail');
         tailElements.forEach((element) => {
-            element.classList.remove('tail');
+            element.classList.remove(BlockType.TAIL);
         });
 
         let newHeadElement = document.querySelector(`[data-x="${snake.head.x}"][data-y="${snake.head.y}"]`);
@@ -71,7 +120,9 @@ let game = {
 
         snake.tail.forEach((tail) => {
             let element = document.querySelector(`[data-x="${tail.x}"][data-y="${tail.y}"]`);
-            element.classList.add(BlockType.TAIL);
+            if (element) {
+                element.classList.add(BlockType.TAIL);
+            }
         });
     },
 
@@ -86,6 +137,24 @@ let game = {
     },
 
     initGame() {
+        let controlButtons = document.querySelectorAll('.control-button');
+
+        controlButtons[0].addEventListener('click', () => {
+            this.fruits.push(getRandomCords());
+        });
+        controlButtons[1].addEventListener('click', () => {
+            this.fruits.pop();
+        });
+
+        controlButtons[2].addEventListener('click', () => {
+            this.speed += 5;
+        });
+        controlButtons[3].addEventListener('click', () => {
+            if (this.speed > 5) {
+                this.speed -= 5;
+            }
+        });
+
         for (let y = 0; y < this.mapHeight; y++) {
             for (let x = 0; x < this.mapWidth; x++) {
                 let block;
@@ -94,6 +163,8 @@ let game = {
                     block = this.createBlock(x, y, BlockType.HEAD);
                 } else if (snake.tail.some((tail) => tail.x === x && tail.y === y)) {
                     block = this.createBlock(x, y, BlockType.TAIL);
+                } else if (game.fruits.some((fruit) => fruit.x === x && fruit.y === y)) {
+                    block = this.createBlock(x, y, BlockType.FRUIT);
                 } else {
                     block = this.createBlock(x, y, BlockType.DEFAULT);
                 }
@@ -121,6 +192,22 @@ let snake = {
 
         this.head.x += delta.x;
         this.head.y += delta.y;
+
+        if (this.head.x < 0) {
+            this.head.x = game.mapWidth - 1;
+        }
+
+        if (this.head.x > game.mapWidth - 1) {
+            this.head.x = 0;
+        }
+
+        if (this.head.y < 0) {
+            this.head.y = game.mapHeight - 1;
+        }
+
+        if (this.head.y > game.mapHeight - 1) {
+            this.head.y = 0;
+        }
 
         this.tail.forEach((tail) => {
             let temp = {...tail};
